ffe59bb8629584be7d453dd2a6ddddb8ce77bd76
narendrathumurothu <narendrathumurothu@gmail.com>
2026-04-25 07:11:47 +0530

    changed to gemma 9bit to llm 3b 70

--- backend/agent.py
+++ backend/agent.py
@@ -14,7 +14,7 @@ from models import Interaction
 load_dotenv()
 
 llm = ChatGroq(
-    model="gemma2-9b-it",
+    model="llama-3.3-70b-versatile",
     api_key=os.getenv("GROQ_API_KEY"),
     temperature=0.3
 )
